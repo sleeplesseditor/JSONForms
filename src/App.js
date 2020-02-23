489c7e7ef69dda70fedf0d03d0d1bdd7f0c74531
Josh Bryson <josh@bryson.me.uk>
2020-02-23 15:41:44 +0000

    Basic navigation setup implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
+import './App.scss';
+
+import LazyLoader from './components/LazyLoader/LazyLoader';
+const FformPage = React.lazy(() => import('./pages/FformPage'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+        <Switch>
+          <Route exact path="/" component={LazyLoader(FformPage)} />
+        </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/FformPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WrapperPage() {
+  return (
+    <div className="App">
+        <h2>Fform Example</h2>
+    </div>
+  );
+}
+
+export default WrapperPage;
