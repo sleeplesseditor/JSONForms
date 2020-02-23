b21c7e23ed022c748a0ed39be60c0958b72ce1f8
Josh Bryson <josh@bryson.me.uk>
2020-02-23 23:30:02 +0000

    JSONFormEngine component made re-useable

--- src/components/JSONFormEngine/JSONFormEngine.jsx
+++ src/components/JSONFormEngine/JSONFormEngine.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import 'react-json-form-engine/dist/css/styles.min.css';
 import { Form, FormEngine } from 'react-json-form-engine';
-import TestEngineSchema from './TestEngineSchema.json';
 
-const instance = new FormEngine(TestEngineSchema); 
+const LoginForm = (props) => {
+  const instance = new FormEngine(props.schema);
 
-const LoginForm = () => (
-  <Form
-    instance={instance}
-    onUpdate={(id, value) => {
-      console.log(`FieldId ${id} was changed to ${value}`);
-      console.log(instance.getValidationResultById(id));
-      console.log(instance.getValidationStatusById(id));
-    }}
-    onSubmit={hasError => {
-      if (hasError) {
-        console.log(instance.getValidationResults()); 
-      }
-      console.log(instance.getModel());                     
-      console.log(instance.serializeModel());         
-    }}
-  />
-);
+  return (
+    <Form
+      instance={instance}
+      onUpdate={(id, value) => {
+        console.log(`FieldId ${id} was changed to ${value}`);
+        console.log(instance.getValidationResultById(id));
+        console.log(instance.getValidationStatusById(id));
+      }}
+      onSubmit={hasError => {
+        if (hasError) {
+          console.log(instance.getValidationResults()); 
+        }
+        console.log(instance.getModel());                     
+        console.log(instance.serializeModel());         
+      }}
+    />
+  )
+};
 
 export default LoginForm;
--- src/pages/JSONFormEnginePage.jsx
+++ src/pages/JSONFormEnginePage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import LoginForm from '../components/JSONFormEngine/JSONFormEngine';
+import TestEngineSchema from '../components/JSONFormEngine/TestEngineSchema.json';
 
 function JSONFormEnginePage() {
   return (
     <div className="App">
         <h2>JSON Form Engine Example</h2>
-        <LoginForm />
+        <LoginForm 
+          schema={TestEngineSchema}
+        />
+        <p>Relies on Bulma HTML semantics – loaded as part of implementation</p>
     </div>
   );
 }
